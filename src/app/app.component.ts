28ac8654307b37f09a5cadf9fc8d84dbbdc5fd88
q <perfec1@rambler.ru>
2021-12-23 20:37:40 +0300

    edd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+export class User{
+  constructor(public name: string,
+              public email: string,
+              public phone: string){}
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+  template: `<div>
+                    <div class="form-group">
+                        <label>Имя</label>
+                        <input class="form-control" name="name" [(ngModel)]="user.name" #name="ngModel" required />
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input class="form-control" type="email" name="email" [(ngModel)]="user.email" #email="ngModel"
+                            required email />
+                    </div>
+                    <div class="form-group">
+                        <label>Телефон</label>
+                        <input class="form-control" name="phone" [(ngModel)]="user.phone" #phone="ngModel"
+                            required pattern="[0-9]{10}" />
+                    </div>
+                    <div class="form-group">
+                        <button [disabled]="name.invalid || email.invalid || phone.invalid"
+                                class="btn btn-default" (click)="addUser()">Добавить</button>
+                    </div>
+              </div>`
 })
 export class AppComponent {
-  title = 'itProger';
+
+  user: User = new User("", "", "");
+  addUser(){
+    console.log(this.user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule }   from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [ AppComponent],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
